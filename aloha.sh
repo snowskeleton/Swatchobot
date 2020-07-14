b13275d384d6ff5bf7ed29ea80791c4f983e841d
Isaac Lyons <drajonknives@gmail.com>
2020-07-14 15:30:48 +0000

    Regular update.

--- aloha.sh
+++ aloha.sh
@@ -4,13 +4,26 @@ player=$1
 aloha=$2
 case $aloha in
 	"joined the game")
-	direction=welcome
+	    direction=welcome
 	;;
 	"left the game")
-	direction=goodbye
+	    direction=goodbye
 	;;
 esac
-	
+
+#case $player in 
+    #"Vyvexe")
+        #if [[ $direction -eq "welcome" ]]
+            #aloha_message="Oh hai insert_player_name_here ^^"
+        #else
+            #aloha_message=$(sqlite3 /reallyfat/MCServers/green/autobot/database.green.db "SELECT * FROM "$direction" ORDER BY RANDOM() LIMIT 1")
+        #fi
+    #;;
+    #*)
+        #aloha_message=$(sqlite3 /reallyfat/MCServers/green/autobot/database.green.db "SELECT * FROM "$direction" ORDER BY RANDOM() LIMIT 1")
+    #;;
+#esac
+
 aloha_message=$(sqlite3 /reallyfat/MCServers/green/autobot/database.green.db "SELECT * FROM "$direction" ORDER BY RANDOM() LIMIT 1")
 message=${aloha_message/'insert_player_name_here'/$player}
 /reallyfat/MCServers/green/autobot/count_players.sh "$aloha"
